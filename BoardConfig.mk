3243b47fea5310071d5e23c2a15878eeb731f7a5
Carlos Sanchez <cfedericosanchez@gmail.com>
2018-01-27 19:59:55 -0300

    cedric: Disable odexing for prebuilts & Disable dexpreopt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,10 @@ TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 BOARD_VENDOR := motorola-qcom
 
+# Dexpreopt
+WITH_DEXPREOPT ?= false
+DONT_DEXPREOPT_PREBUILTS := true
+
 WITH_LINEAGE_CHARGER := false
 
 # AIDs and CAPS
